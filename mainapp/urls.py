e199ae7c7d072f23ee2ebda9a7c2cef1ed234f70
prog <nik.zakharenko.1999@mail.ru>
2023-05-25 23:43:45 +0700

    third

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("pharmacies/", views.pharmacy,name='pharmacies'),
     path("", views.index, name="index"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,8 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 
 def index(request):
-    template = loader.get_template('aboutme.html')
+    template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def pharmacy(request):
+    template = loader.get_template('pharmacy.html')
+    context = {
+        'pharmacies' : Pharmacy.objects.all(),
+        'title': 'Список аптек'
+    }
+    return HttpResponse(template.render(context, request))
+
+
