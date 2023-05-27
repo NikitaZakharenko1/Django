af51d1e4eb0a05dd17164f507eaed906b0e424e4
prog <nik.zakharenko.1999@mail.ru>
2023-05-27 16:09:07 +0700

    four

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("pharmacies/", views.pharmacy,name='pharmacies'),
+    path("cures/", views.cures,name='cures'),
+    path("cure/<int:dk>",views.cure,name='cure'),
     path("", views.index, name="index"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import *
 
@@ -15,6 +15,27 @@ def pharmacy(request):
     context = {
         'pharmacies' : Pharmacy.objects.all(),
         'title': 'Список аптек'
+    }
+    return HttpResponse(template.render(context, request))
+
+def cures(request):
+    template = loader.get_template('cures.html')
+    context = {
+        'cures' : Cure.objects.all(),
+        'title': 'Список лекарств'
+    }
+    return HttpResponse(template.render(context, request))
+
+def cure(request,dk):
+    try:
+        cure = Cure.objects.get(id=dk)
+    except Cure.DoesNotExist:
+        raise Http404("Лекарство с кодом " + str(dk) + " не найдено!")
+
+    template = loader.get_template('cure.html')
+    context = {
+        'cure' : cure,
+        'title': 'Список лекарств'
     }
     return HttpResponse(template.render(context, request))
 
