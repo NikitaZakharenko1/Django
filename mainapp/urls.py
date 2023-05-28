338708782463448ee6b8b1a6796127b1e22dde2a
prog <nik.zakharenko.1999@mail.ru>
2023-05-28 14:01:12 +0700

    six

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("pharmacy/<int:kp>/delete/",views.del_pharmacy,name='del_pharmacy'),
     path("cures/", views.cures,name='cures'),
     path("cure/<int:dk>",views.cure,name='cure'),
+    path("register/", views.register_user,name='register'),
+    path("login/", views.login_user, name='login'),
+    path("logout/", views.logout_user, name='logout'),
     path("", views.index, name="index"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect,HttpResponseForbidden
 from django.urls import reverse
 from django.contrib import  messages
+from django.contrib.auth import *
+from django.contrib.auth.forms import *
 from django.template import loader
 from .models import *
 from .forms import *
@@ -17,7 +19,8 @@ def pharmacy(request):
     template = loader.get_template('pharmacy.html')
     context = {
         'pharmacies' : Pharmacy.objects.all(),
-        'title': 'Список аптек'
+        'title': 'Список аптек',
+        'power_user': request.user.groups.filter(name='admin').exists()
     }
     return HttpResponse(template.render(context, request))
 
@@ -35,50 +38,59 @@ def cure(request,dk):
     return HttpResponse(template.render(context, request))
 
 def new_pharmacy(request):
-    if request.method == "POST":
-        form = PharmacyForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('pharmacies'))
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = PharmacyForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('pharmacies'))
+        else:
+            form = PharmacyForm()
+        template = loader.get_template('pharmacy_form.html')
+        context = {
+            'form': form,
+            'title':'добавление аптеки'
+        }
+        return HttpResponse(template.render(context,request))
     else:
-        form = PharmacyForm()
-    template = loader.get_template('pharmacy_form.html')
-    context = {
-        'form': form,
-        'title':'добавление аптеки'
-    }
-    return HttpResponse(template.render(context,request))
+        return HttpResponseForbidden("<h1>Доступ запрещён</h1>")
 
 def edit_pharmacy(request,kp):
-    try:
-        pharmacy = Pharmacy.objects.get(id=kp)
-    except Pharmacy.DoesNotExist:
-        raise Http404("Аптека с кодом " + str(kp) + " не найдена")
+    if request.user.is_authenticated:
+        try:
+            pharmacy = Pharmacy.objects.get(id=kp)
+        except Pharmacy.DoesNotExist:
+            raise Http404("Аптека с кодом " + str(kp) + " не найдена")
 
-    if request.method == 'POST':
-        form = PharmacyForm(request.POST, instance=pharmacy)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('pharmacies'))
+        if request.method == 'POST':
+            form = PharmacyForm(request.POST, instance=pharmacy)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('pharmacies'))
+        else:
+            form = PharmacyForm(instance=pharmacy)
+
+        template = loader.get_template('pharmacy_form.html')
+        context = {
+            'form':form,
+            'title':'редактирование аптеки'
+        }
+        return  HttpResponse(template.render(context,request))
     else:
-        form = PharmacyForm(instance=pharmacy)
-
-    template = loader.get_template('pharmacy_form.html')
-    context = {
-        'form':form,
-        'title':'редактирование аптеки'
-    }
-    return  HttpResponse(template.render(context,request))
+        return HttpResponseForbidden("<h1>Доступ запрещён</h1>")
 
 def del_pharmacy(request, kp):
-    try:
-        pharmacy = Pharmacy.objects.get(id=kp)
-    except Pharmacy.DoesNotExist:
-        raise Http404("Аптека с кодом " + str(kp) + " не найдена")
-    m = f"Аптека {pharmacy.name} удалена"
-    pharmacy.delete()
-    messages.error(request,m)
-    return HttpResponseRedirect(reverse('pharmacies'))
+    if request.user.is_authenticated:
+        try:
+            pharmacy = Pharmacy.objects.get(id=kp)
+        except Pharmacy.DoesNotExist:
+            raise Http404("Аптека с кодом " + str(kp) + " не найдена")
+        m = f"Аптека {pharmacy.name} удалена"
+        pharmacy.delete()
+        messages.error(request,m)
+        return HttpResponseRedirect(reverse('pharmacies'))
+    else:
+        return HttpResponseForbidden("<h1>Доступ запрещён</h1>")
 
 def cures(request):
     template = loader.get_template('cures.html')
@@ -87,3 +99,43 @@ def cures(request):
         'title': 'Список лекарств'
     }
     return HttpResponse(template.render(context, request))
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserCreationForm()
+    template = loader.get_template('user_form.html')
+    context = {
+        'form': form,
+        'title':'Создание пользоавтеля'
+    }
+    return HttpResponse(template.render(context,request))
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            userpass = form.cleaned_data['password']
+            user = authenticate(username=username,password=userpass)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = AuthenticationForm()
+    template = loader.get_template('user_form.html')
+    context = {
+        'form': form,
+        'title':'Вход в систему'
+    }
+    return HttpResponse(template.render(context,request))
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Вы вышли из системы')
+    return HttpResponseRedirect(reverse('index'))
